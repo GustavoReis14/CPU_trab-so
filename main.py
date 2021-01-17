1047d9dae3ca4aa5994be8c8aec098e660e14773
GustavoReis14 <gustavo.reis1402@gmail.com>
2021-01-17 12:58:42 -0300

    Antiga main agora atua dentro do Sistema Operacional

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-from cpu import Cpu
+from sistemaOperacional import SistemaOperacional
 
-file = open('inst.txt', 'r')
-
-cpu = Cpu()
-
-cpu.altera_estado()
-cpu.altera_programa(file)
-cpu.altera_dados()
-
-while cpu.interrupcao() == 'normal':
-    cpu.executa()
-
-
-resposta = 0
-print(f'CPU parou na instrucao {cpu.instrucao()}')
-print(f'O valor de m[{resposta}] eh {cpu.get_mem_dados_indice(resposta)}')
+SistemaOperacional()
