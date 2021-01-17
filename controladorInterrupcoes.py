81f4421e80038df4cffde640331b8b78812b1208
GustavoReis14 <gustavo.reis1402@gmail.com>
2021-01-17 14:55:56 -0300

    alterado para incrementar pc

--- controladorInterrupcoes.py
+++ controladorInterrupcoes.py
@@ -3,7 +3,16 @@ from cpu import Cpu
 class ControladorInterrupcoes:
   
 
-  def execucao_cpu(self, cpu):
+  def execucao_cpu(self, cpu, so):
+    comando = ''
     while cpu.interrupcao() == 'normal':
       cpu.executa()
+      if cpu.interrupcao() != 'normal':
+        revolvido = so.resolve_instrucao(cpu.instrucao())
+        if revolvido:
+          cpu.altera_estado()
+
+
+
+
     
