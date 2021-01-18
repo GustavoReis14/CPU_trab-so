5380ed3194ed8ec60e72830d3a36de7a1fc2e5c7
GustavoReis14 <gustavo.reis1402@gmail.com>
2021-01-18 11:37:53 -0300

    salva a cpu  no job atual e coloca em estado de dormencia

--- sistemaOperacional.py
+++ sistemaOperacional.py
@@ -1,24 +1,40 @@
 from cpu import Cpu
 from controladorInterrupcoes import ControladorInterrupcoes
+from timer import Timer
+from job import Job
 
 class SistemaOperacional:
   def __init__(self):
-    self._file = open('inst.txt', 'r')
 
     #instrucoes
     self._instrucoes_dic = {'PARA': self.__para, "LE": self.__le, "GRAVA": self.__grava}
 
+    #timer
+    self._timer = Timer()
+
+    #Jobs
+    self._lista_jobs = []
+    self._job1 = Job(open('./programas/prog1.txt', 'r'), self._timer.tempo_atual())
+    self._job2 = Job(open('./programas/prog2.txt', 'r'), self._timer.tempo_atual())
+
+    self._lista_jobs.extend([self._job1, self._job2])
+
+    self._job_atual = -1
+
     #inicia Cpu
     self._cpu = Cpu()
-    self._cpu.altera_estado()
-    self._cpu.altera_programa(self._file)
-    self._cpu.altera_dados()
-    self._file.close()
+
+    for index, job in enumerate(self._lista_jobs, start=0):
+      self._job_atual = index
+      self._cpu.altera_estado()
+      self._cpu.altera_programa(job.getPrograma())
+      self._cpu.altera_dados()
+
 
     #inicia e roda o controlador
     self._controlador = ControladorInterrupcoes()
-    self._controlador.execucao_cpu(self._cpu, self)
-
+    self._controlador.execucao_cpu(self._cpu, self, self._timer)
+    
 
   def __para(self):
     print("Exit")
@@ -39,5 +55,13 @@ class SistemaOperacional:
 
   def resolve_instrucao(self, instrucao):
     if instrucao in self._instrucoes_dic:
+      if instrucao != 'PARA':
+        self._cpu.estado_dormencia()
+        self._salvaCpu()
+
       return self._instrucoes_dic[instrucao]()
       
+  def _salvaCpu(self):
+    self._lista_jobs[self._job_atual].setPc(self._cpu.getPc())  
+    self._lista_jobs[self._job_atual].setAcumulador(self._cpu.getAcumulador()) 
+    self._lista_jobs[self._job_atual].setMem_dados(self._cpu.getMem_dados())
